7bf51dec01b399cb5b684d5b35ada0bbd1a20838
Matteo Cominetti <matteo@cominetti.org>
2016-07-05 10:43:34 +0100

    added references as nuget packages, fixed pkg.json to load dependencies into Dynamo

--- HelloDynamo/Hello/SampleFunctions.cs
+++ HelloDynamo/Hello/SampleFunctions.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Autodesk.DesignScript.Runtime;
 
-namespace Hello
+namespace HelloDynamo.Hello
 {
-  public static class SampleFunctions
+  /// <summary>
+  /// In order to be executed by the NodeModel AstFactory.BuildFunctionCall 
+  /// these methods have to be in a separate assembly and be loaded by Dynamo separately
+  /// File pkg.json defines which dll are loaded
+  /// </summary>
+  [IsVisibleInDynamoLibrary(false)]
+  public class SampleFunctions
   {
     public static double MultiplyTwoNumbers(double a, double b)
     {
