c341b827e504e61efb75200f243cc02583c32388
Matteo Cominetti <matteo@cominetti.org>
2016-07-05 12:06:33 +0100

    Added sample for custom GUI node

---
+++ HelloDynamo/HelloNodeModel/HelloGui.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Dynamo.Graph.Nodes;
+using HelloDynamo.Hello;
+using ProtoCore.AST.AssociativeAST;
+
+namespace HelloDynamo.HelloNodeModel
+{
+  /// <summary>
+  /// Sample NodeModel with custom Gui
+  /// </summary>
+  [NodeName("HelloGui")]
+  [NodeDescription("Example Node Model, multiplies A x the value of the slider")]
+  [NodeCategory("HelloDynamo")]
+  [InPortNames("A")]
+  [InPortTypes("double")]
+  [InPortDescriptions("Number A")]
+  [OutPortNames("C")]
+  [OutPortTypes("double")]
+  [OutPortDescriptions("Product of AxSlider")]
+  [IsDesignScriptCompatible]
+  public class HelloGui : NodeModel
+  {
+    private double _sliderValue;
+
+
+    public double SliderValue
+    {
+      get { return _sliderValue; }
+      set
+      {
+        _sliderValue = value; 
+        RaisePropertyChanged("SliderValue");
+        OnNodeModified(false);
+      }
+    }
+
+    public HelloGui()
+    {
+      RegisterAllPorts();
+    }
+
+    public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
+    {
+      if (!HasConnectedInput(0))
+      {
+        return new[] { AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), AstFactory.BuildNullNode()) };
+      }
+      var sliderValue = AstFactory.BuildDoubleNode(SliderValue);
+      var functionCall =
+        AstFactory.BuildFunctionCall(
+          new Func<double, double, double>(SampleFunctions.MultiplyTwoNumbers),
+          new List<AssociativeNode> { inputAstNodes[0], sliderValue });
+
+      return new[] { AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), functionCall) };
+    }
+  }
+}
---
+++ HelloDynamo/HelloNodeModel/HelloGuiNodeView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dynamo.Controls;
+using Dynamo.Wpf;
+
+namespace HelloDynamo.HelloNodeModel
+{
+  public class HelloGuiNodeView : INodeViewCustomization<HelloGui>
+  {
+    public void CustomizeView(HelloGui model, NodeView nodeView)
+    {
+     var slider = new Slider();
+      nodeView.inputGrid.Children.Add(slider);
+      slider.DataContext = model;
+    }
+
+    public void Dispose()
+    {
+    }
+  }
+}
